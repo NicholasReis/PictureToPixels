32d94a6754c620d2cbb462d2a3edf7e2001a9538
NicholasReis <NicholasCReis@gmail.com>
2022-03-04 14:53:43 -0800

    Created a GUI file to hold all layouts
    Frankly I am impressed with myself for being able to figure this one out. It does act as a sort of middle man, but is more organized by what you should be seeing on the screen. I think this is the best way to do it. I will go back and rework the UI, but for now I am going to get that save button working.

---
+++ GUI.java
@@ -0,0 +1,141 @@
+import javafx.event.EventHandler;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.scene.image.Image;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.paint.Color;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javafx.scene.image.WritableImage;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.PixelReader;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+public class GUI{
+    Stage stage;
+    
+    public GUI(Stage tempStage){
+        stage = tempStage;
+    }
+
+    public void welcomeScreen(){
+        //Label to welcome
+        Label welcomeLabel = new Label("Welcome. To continue please choose a photo.");
+        //Button to select a photo through file explorer
+        Button imageSelectButton = new Button("Select photo");
+        //Calls photoChoice with the related action event when the button is pressed
+        imageSelectButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override  
+                public void handle(ActionEvent arg0) {
+                    selectionScreen();
+                }  
+            });
+
+        //Adds the label and button to a vertical box
+        VBox group = new VBox(10, welcomeLabel, imageSelectButton);
+        //centers the vertical box
+        group.setAlignment(Pos.CENTER);
+
+        stage.setScene(new Scene(group));
+    }
+
+    public void selectionScreen(){
+        PhotoChoice PC = new PhotoChoice();
+        Pixelator pixelator = new Pixelator();
+
+        //Creates a new imageview to display the writable image
+        ImageView pixelizedImage = new ImageView(PC.getWritableImage());
+
+        //Creates a button to select a new image
+        Button imageSelectButton = new Button("Select photo");
+        //On button click calls photoChoice with the related action event as a parameter
+        imageSelectButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override  
+                public void handle(ActionEvent arg0) {
+                    selectionScreen();
+                }  
+            });
+
+        //sets the window size to fit 1200x800
+        pixelizedImage.setFitHeight(500);
+        pixelizedImage.setFitWidth(600);
+        //preserves ratio so it doesn't warp image
+        pixelizedImage.setPreserveRatio(true);
+
+        //Creates a slider between 1 and 64 for the pixel ratio to pixelize,
+        //@2 it would take the average of every 2x2 pixels and average the colors,
+        //@40 it would take the average of ever 40/40 pixels and average the colors, etc
+        Slider pixelizeMeter = new Slider(1,64, 1);
+
+        //Adds a listener to check the value of the slider
+        pixelizeMeter.valueProperty().addListener(
+            (observable, oldvalue, newvalue) ->
+            {
+                //sets the bitsize to the new value of the slider
+                final int bitSize = newvalue.intValue();
+                //calls pixelize
+                final WritableImage pixelImage = pixelator.pixelize(PC.getWritableImage(), bitSize);
+                //sets the imageview to display the now pixelized image (which is why it's global)
+                pixelizedImage.setImage(pixelImage);
+            } );
+
+        //Label for the slider
+        Label sliderLabel = new Label("Pixelize Meter");
+
+        //Button to save
+        Button saveButton = new Button("Save");
+        //When the button is pressed it calls saveImage
+
+        //saveButton.setOnAction(this::saveImage, pixelizedImage);!!!
+        //Adds the label and slider to a vertical box
+        VBox meterAndLabel = new VBox(2, sliderLabel, pixelizeMeter);
+        //sets the label and slider to the bottome center
+        meterAndLabel.setAlignment(Pos.BOTTOM_CENTER);
+
+        //creates a horizontal box that contains the image selection button, slider and label vbox, and save button
+        HBox underImage = new HBox(10, imageSelectButton, meterAndLabel, saveButton);
+
+        //sets horizontal box to the bottome center (underneath the imageview of the selected picture)
+        underImage.setAlignment(Pos.BOTTOM_CENTER);
+
+        //new vertical box to store both the imageview and the toolbar
+        VBox group = new VBox(10, pixelizedImage, underImage);
+        //sets the new vbox to the center of the window
+        group.setAlignment(Pos.BASELINE_CENTER);
+
+        stage.setScene(new Scene(group));
+    }
+
+    public void imageScreen(){
+        Button imageSelectButton = new Button("Select photo");
+        //On button click calls photoChoice with the related action event as a parameter
+        imageSelectButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override  
+                public void handle(ActionEvent arg0) {
+                    selectionScreen();
+                }  
+            });
+        //Adds the label and button to a vertical box
+        VBox group = new VBox(10, imageSelectButton);
+        //centers the vertical box
+        group.setAlignment(Pos.CENTER);
+        //Adds the label and button to a vertical box
+        group = new VBox(10, imageSelectButton);
+        //centers the vertical box
+        group.setAlignment(Pos.CENTER);
+
+        stage.setScene(new Scene(group));
+    }
+}
--- PictureToPixels.java
+++ PictureToPixels.java
@@ -32,114 +32,14 @@ public class PictureToPixels extends Application{
     }
 
     @Override
-    public void start(Stage theStage){
-        Stage stage = theStage;
-        //Label to welcome
-        Label welcomeLabel = new Label("Welcome. To continue please choose a photo.");
-        //Button to select a photo through file explorer
-        Button imageSelectButton = new Button("Select photo");
-        //Calls photoChoice with the related action event when the button is pressed
-        imageSelectButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override  
-                public void handle(ActionEvent arg0) {
-                    drawGUI(stage);
-                }  
-            });
-
-        //Adds the label and button to a vertical box
-        VBox group = new VBox(10, welcomeLabel, imageSelectButton);
-        //centers the vertical box
-        group.setAlignment(Pos.CENTER);
-
-        //puts the vbox into the scene
-        Scene scene = new Scene(group, 300,100);
+    public void start(Stage stage){
+        GUI gui = new GUI(stage);
         //sets the title for the window
         stage.setTitle("Picture to Pixel");
         //puts the scene into the stage
-        stage.setScene(scene);
+        gui.welcomeScreen();
         //Shows the stage
         stage.show();
     }
 
-    private void drawGUI(Stage stage){
-        PhotoChoice PC = new PhotoChoice();
-        PhotoEditor PE = new PhotoEditor();
-
-        //Creates a new imageview to display the writable image
-        ImageView pixelizedImage = new ImageView(PC.getWritableImage());
-
-        //Creates a button to select a new image
-        Button imageSelectButton = new Button("Select photo");
-        //On button click calls photoChoice with the related action event as a parameter
-        imageSelectButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override  
-                public void handle(ActionEvent arg0) {
-                    drawGUI(stage);
-                }  
-            });
-
-            VBox group;
-        if(PC.imageChosen()){
-            //sets the window size to fit 1200x800
-            pixelizedImage.setFitHeight(500);
-            pixelizedImage.setFitWidth(600);
-            //preserves ratio so it doesn't warp image
-            pixelizedImage.setPreserveRatio(true);
-
-            //Creates a slider between 1 and 64 for the pixel ratio to pixelize,
-            //@2 it would take the average of every 2x2 pixels and average the colors,
-            //@40 it would take the average of ever 40/40 pixels and average the colors, etc
-            Slider pixelizeMeter = new Slider(1,64, 1);
-
-            //Adds a listener to check the value of the slider
-            pixelizeMeter.valueProperty().addListener(
-                (observable, oldvalue, newvalue) ->
-                {
-                    //sets the bitsize to the new value of the slider
-                    final int bitSize = newvalue.intValue();
-                    //calls pixelize
-                    final WritableImage pixelImage = PE.pixelize(PC.getWritableImage(), bitSize);
-                    //sets the imageview to display the now pixelized image (which is why it's global)
-                    pixelizedImage.setImage(pixelImage);
-                } );
-
-            //Label for the slider
-            Label sliderLabel = new Label("Pixelize Meter");
-
-            //Button to save
-            Button saveButton = new Button("Save");
-            //When the button is pressed it calls saveImage
-
-            //saveButton.setOnAction(this::saveImage, pixelizedImage);!!!
-            //Adds the label and slider to a vertical box
-            VBox meterAndLabel = new VBox(2, sliderLabel, pixelizeMeter);
-            //sets the label and slider to the bottome center
-            meterAndLabel.setAlignment(Pos.BOTTOM_CENTER);
-
-            //creates a horizontal box that contains the image selection button, slider and label vbox, and save button
-            HBox underImage = new HBox(10, imageSelectButton, meterAndLabel, saveButton);
-
-            //sets horizontal box to the bottome center (underneath the imageview of the selected picture)
-            underImage.setAlignment(Pos.BOTTOM_CENTER);
-
-            //new vertical box to store both the imageview and the toolbar
-            group = new VBox(10, pixelizedImage, underImage);
-            //sets the new vbox to the center of the window
-            group.setAlignment(Pos.BASELINE_CENTER);
-
-            //sets the padding to be 5 on the top left and right, but 30 on the bottom for more space
-            
-        }else{
-            //Adds the label and button to a vertical box
-            group = new VBox(10, imageSelectButton);
-            //centers the vertical box
-            group.setAlignment(Pos.CENTER);
-
-        }
-
-        group.setPadding(new Insets(10, 10, 10, 10));
-        //sets the new layout in a new scene and the new scene into the stage
-        stage.setScene(new Scene(group));
-
-    }
 }
